5ba2b9bbfb81997d40be83d2029b29cb5f87391d
ParkSeongju <parksj914@naver.com>
2020-08-28 23:01:22 +0900

    Users-dispach

--- src/Users.js
+++ src/Users.js
@@ -1,32 +1,58 @@
-import React, {useState, useEffect} from 'react';
+import React, {useReducer, useEffect} from 'react';
 import axios from 'axios';
 
-function Users() {
-    const [users, setUsers] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+function reducer(state, action) {
+    switch (action.type) {
+        case 'LOADING':
+            return {
+                loading: true,
+                data: null,
+                error: null
+            };
+        case 'SUCCESS':
+            return {
+                loading: false,
+                data: action.data,
+                error: null
+            };
+        case 'ERROR':
+            return {
+                loading: false,
+                data: null,
+                error: action.error
+            };
+        default:
+            throw new Error(`Unhandled cation type: ${action.type}`);
+    }
+}
 
-    const fetchUsers = async ()=>{
+function Users() {
+
+    const [state, dispatch] = useReducer(reducer, {
+       loading: false,
+       data: null,
+       error: null
+    });
+
+    const fetchUsers = async () => {
+        dispatch({type: 'LOADING'});
         try{
-            //요청이 시작될 때 error, users 초기화
-            setError(null);
-            setUsers(null);
-            // loading 상태 true 로
-            setLoading(true);
             const response = await axios.get(
                 'https://jsonplaceholder.typicode.com/users'
             );
-            setUsers(response.data);
+            dispatch({type: 'SUCCESS', data: response.data});
         } catch (e) {
-            setError(e);
+            dispatch({type: 'ERROR', error: e});
         }
-        setLoading(false);
-    };
+
+    }
 
     useEffect( () => {
             fetchUsers();
         }, []
     );
+
+    const {loading, data: users, error} = state;
 
     if(loading) return <div>로딩중..</div>;
     if(error) return <div>에러가 발생했습니다.</div>;
