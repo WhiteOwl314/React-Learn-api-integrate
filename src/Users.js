97137c80b9b60f2734f173531e36e22c46eecb0c
ParkSeongju <parksj914@naver.com>
2020-08-29 01:25:39 +0900

    User-적용

--- src/Users.js
+++ src/Users.js
@@ -1,6 +1,7 @@
-import React, {useReducer, useEffect} from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 import useAsync from './useAsync';
+import User from './User';
 
 // useAsync 에서는 Promise 의 결과를 바로 data 에 담기 때문에,
 // 요청을 한 이후 response 에서 data 추출하여 바환하는 함수 따로 만듬
@@ -13,7 +14,7 @@ async function getUsers() {
 }
 
 function Users() {
-
+    const [userId, setUserId] = useState(null)
     const [state, refetch] = useAsync(getUsers, [], true);
 
     const {loading, data: users, error} = state;
@@ -26,12 +27,19 @@ function Users() {
         <>
             <ul>
                 {users.map(user =>
-                    (<li key={user.id}>
-                        {user.username} ({user.name})
-                    </li>)
+                    (
+                        <li
+                            key={user.id}
+                            onClick={()=> setUserId(user.id)}
+                            style={{cursor: "pointer"}}
+                        >
+                            {user.username} ({user.name})
+                        </li>
+                    )
                 )}
             </ul>
             <button onClick={refetch}>다시 불러오기</button>
+            {userId && <User id={userId}/>}
         </>
     )
 
